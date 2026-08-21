627bcc91f0c3508c872a2a56ad50608e4ff77e57
giolajide <141545135+giolajide@users.noreply.github.com>
2026-08-20 22:01:26 -0500

    Add tests for non-positive and duplicate values

--- slurm_waitmap/tests/test_labels.py
+++ slurm_waitmap/tests/test_labels.py
@@ -10,6 +10,17 @@ def test_define_labels():
         define_labels([1, 4], [1])
     with pytest.raises(ValueError):
         define_labels([1], [1])
+    
+    ##non-positive values
+    with pytest.raises(ValueError):
+        define_labels([0, 2], [1, 2])
+    with pytest.raises(ValueError):
+        define_labels([1, 2], [-1, 2])
+    ##duplicate/non-ascending values
+    with pytest.raises(ValueError):
+        define_labels([1, 1, 2], [1, 2])
+    with pytest.raises(ValueError):
+        define_labels([2, 1], [1, 2])
     #valid cpu and time bins
     cpu_labels, cpu_bin_edges, time_labels, time_bin_edges = define_labels(
             [1, 2, 7, 14, 21, 28],
@@ -17,7 +28,7 @@ def test_define_labels():
             )
     assert cpu_labels == ["<1", "1-1", "2-6", "7-13", "14-20", "21-27", "28+"]
     assert cpu_bin_edges  == [-inf, 1, 2, 7, 14, 21, 28, inf]
-    assert time_labels  == ["<1", "1-1", "2-23", "24-47", "48+"]
+    assert time_labels == ["<1", "1-<2", "2-<24", "24-<48", "48+"]
     assert time_bin_edges  == [-inf, 1, 2, 24, 48, inf]
     ##no need to test invalid types, as these are already
     ##screened out by the main() function, by setting type=int in argparse
